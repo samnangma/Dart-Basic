da0f884ecba0a8be304193792c7da230799bfa98
samnangma <masamnang6@gmail.com>
2023-08-22 11:44:05 +0700

    first commit

---
+++ bin/project.dart
@@ -0,0 +1,20 @@
+import 'package:project/project.dart' as project;
+
+// void main(List<String> arguments) {
+//   print('Hello world: ${project.calculate()}!');
+// }
+
+main() {
+  int i = 10 ;
+  double money = 200.4;
+  double score = .90;
+
+  String name = "Samnang";
+  bool isValid = true;
+
+  var h = 12;
+
+  print('Hello World: $i ');
+  print('Money $money $score');
+  print("Name $name");
+}
