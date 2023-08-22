2aea30e687091ac00ba2b2159ddcb3e26a5fc554
samnangma <masamnang6@gmail.com>
2023-08-22 14:22:01 +0700

    Basic Data Type

--- bin/project.dart
+++ bin/project.dart
@@ -5,16 +5,54 @@ import 'package:project/project.dart' as project;
 // }
 
 main() {
-  int i = 10 ;
-  double money = 200.4;
-  double score = .90;
 
-  String name = "Samnang";
-  bool isValid = true;
+  // final Name = "Dara"; // one time value assign
+  // int i = 10 ;
+  // double money = 200.4;
+  // double score = .90;
+  //
+  // String name = "Samnang";
+  // bool isValid = true;
+  //
+  // var h = 12; // var is dynamic but can not change the data type
+  // // h = "asdas";
+  //
+  // dynamic as = 12; // dynamic can change whatever you want
+  // as = "123";
+  // as = false;
+  //
+  //
+  //
+  // print('Hello World: $i ');
+  // print('Money $money $score');
+  // print("Name $name");
 
-  var h = 12;
 
-  print('Hello World: $i ');
-  print('Money $money $score');
-  print("Name $name");
+  // int ? a = 5 ;
+  // int b = 2;
+  //
+  // a ??= 30 ; // ?? if a = null a = 30 ;
+  //
+  // print(a);
+
+  // print (a is int );
+  // print (a is! int );
+  //
+  // print('a/b ${a/b}');
+  // print("a+b ${a+b}");
+  // print("a-b ${a-b}");
+  // print("a/b ${a ~/b }"); // ~/ Divide, returning an integer result
+  // print("a/b ${a % b }"); // % Get the remainder of an integer division
+
+  // int? a = 30;
+  //
+  // // print( a > 15 ? " a is greater then 15 " : " a is lower than 15 " );
+  //
+  // print ( a ?? 40 ); // if a = null a ng yk 40 print mk
+
+  //Control Flow
+
+
+
+
 }
