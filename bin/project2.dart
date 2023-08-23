85a493196e131b7406432bf0329a97a5a1df900d
samnangma <masamnang6@gmail.com>
2023-08-23 10:09:51 +0700

    Customize Print Statements

---
+++ bin/project2.dart
@@ -0,0 +1,15 @@
+import 'dart:core';
+
+// Customize the Print Statements
+
+void main (){
+  var name = "Nara";
+  var age = 25;
+  var salary = 2500;
+
+  var pi =3.14231;
+  
+  print("this value of pi ${pi.toStringAsFixed(3)}");
+
+  print('Name: $name , Age: $age , Salary: $salary');
+}
