8b975845b9571908b40b73fd856ca07e1773cc48
Samnang MA <masamnang6@gmail.com>
2023-08-23 19:56:57 +0700

    ternary operator

---
+++ bin/ternary.dart
@@ -0,0 +1,40 @@
+import 'dart:core';
+
+// The ternary operator is a shorthand conditional expression in C , C++ and some other programming languages
+// it allows you to write a one-line conditional statemnent instead of an if-else block
+
+// the basic syntax is :
+// condition ? expression_if_true : expression_if_false;
+
+// syntax 2 : expression1 ?? experssiobn 2
+
+void main() {
+  int num = 10;
+  var isEven = num % 2 == 0 ? true : false;
+  print(isEven);
+
+  var ans = 20;
+  ans == 20 ? print("Answer is 20") : print("that's wrong");
+
+  int number = 20;
+  String result = number > 30
+      ? "Positive"
+      : number < 30
+          ? "Negative"
+          : "Zero";
+  String result1 = number > 10
+      ? "Positive"
+      : number < 10
+          ? "Negative"
+          : "Zero";
+  String result2 = number > 30
+      ? "Positive"
+      : number < 10
+          ? "Negative"
+          : "Zero";
+
+  print(result);
+  print(result1);
+  print(result2);
+
+}
